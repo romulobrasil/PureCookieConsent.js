34a96277df30a8006f6ded63294200eb82ba7281
Rômulo Brasil <sir.rommer@gmail.com>
2023-01-08 09:34:47 -0300

    Settings CSS

--- purecookieconsent.js
+++ purecookieconsent.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Pure Cookie Consent JS: purecookieconsent.js v1.0
+ * Pure Cookie Consent JS: purecookieconsent.js v1.0.1
  * https://romulobrasil.com
  * Copyright (c) 2023 Rômulo Brasil
  * ========================================================================
@@ -10,17 +10,23 @@ const PureCookieConsent = {
     txt: 'Este site usa cookies para garantir a melhor experiência do usuário. Ao continuar navegando no site, você concorda com o uso de cookies.',
     btnTxt: 'Eu concordo',
     styleBar: {
-      'width': '100%',
+      'position': 'fixed',
+      'left': '0',
+      'right': '0',
+      'bottom': '15px',
+      'margin-left': 'auto',
+      'margin-tighe': 'auto',
+      'font-family': '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
+    },
+    styleCon: {
       'display': 'flex',
       'justify-content': 'space-between',
       'align-items': 'center',
-      'position': 'fixed',
-      'left': '0',
-      'bottom': '0',
-      'padding': '15px',
-      'background-color': '#ecf0f1',
+      'background-color': '#FFF',
       'color': '#5c6d7e',
-      'font-family': '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
+      'padding': '1.5em',
+      'box-shadow': '0px 0px 64px rgb(0 0 0 / 10%)',
+      'border-radius': '5px',
     },
     styleTxt: {
       'margin': '0',
@@ -30,32 +36,40 @@ const PureCookieConsent = {
     },
     styleBtn: {
       'border': '0',
-      'padding': '6px 10px',
+      'padding': '0 46px',
       'background-color': '#5c6d7e',
       'color': '#fff',
       'cursor': 'pointer',
       'font-size': '15px',
+      'font-weight': '500',
+      'line-height': '40px',
+      'letter-spacing': '1px',
+      'border-radius': '0.25rem',
     },
   },
   show(){
     const args = (arguments[0]) ? arguments[0] : false;
     if(this.getCookie('cookieConsent') != null) {
       const cookieConsentBar = document.createElement('div');
+      const cookieConsentCon = document.createElement('div');
       const cookieConsentTxt = document.createElement('p');
       const agreeBtn = document.createElement('button');
       
       cookieConsentBar.id = 'pureCookieConsent';
       cookieConsentBar.classList.add('pure-cookie-consent');      
+      cookieConsentCon.classList.add('container', 'pure-cookie-consent_box');      
       cookieConsentTxt.classList.add('pure-cookie-consent__txt');
       agreeBtn.classList.add('pure-cookie-consent__btn');
       
       cookieConsentTxt.innerHTML = (args['txt']) ? args['txt'] : this.default['txt'];
       agreeBtn.innerHTML = (args['btnTxt']) ? args['btnTxt'] : this.default['btnTxt'];
       
-      cookieConsentBar.append(cookieConsentTxt, agreeBtn);
+      cookieConsentCon.append(cookieConsentTxt, agreeBtn);
+      cookieConsentBar.appendChild(cookieConsentCon);
 
       if(args['style'] === undefined){
         this.css(cookieConsentBar, this.default['styleBar']);
+        this.css(cookieConsentCon, this.default['styleCon']);
         this.css(cookieConsentTxt, this.default['styleTxt']);
         this.css(agreeBtn, this.default['styleBtn']);
       }
